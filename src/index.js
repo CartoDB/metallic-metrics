c7df80d4b536a50191f6e4aac84d7d11e20ae53b
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-05-27 11:19:48 +0200

    Move factory to its own module

--- src/index.js
+++ src/index.js
@@ -1,26 +1,2 @@
-import { FactoryInterface } from 'metallic-interfaces'
-import Metrics from './metrics'
-import DisabledMetrics from './disabled-metrics'
-import StatsD from 'node-statsd'
-import defaults from './defaults'
-
+export { default } from './metrics-factory'
 export { default as MetricsInterface } from './metrics-interface'
-
-export default class MetricsFactory extends FactoryInterface {
-  static create (logger, opts) {
-    const options = { ...defaults, ...opts }
-
-    if (!options.enabled) {
-      return new DisabledMetrics()
-    }
-
-    const host = options.host
-    const port = options.port
-    const interval = options.interval
-    const prefix = options.prefix
-
-    const statsd = new StatsD(host, port, prefix)
-
-    return new Metrics(statsd, interval, logger)
-  }
-}
