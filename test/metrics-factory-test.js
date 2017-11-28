15f1f02f1fccb8b8d709d434a731b34b5d27ecbe
Daniel García Aubert <danielgarciaaubert@gmail.com>
2017-11-28 19:49:13 +0100

    Use default values

--- test/metrics-factory-test.js
+++ test/metrics-factory-test.js
@@ -1,27 +1,16 @@
 import assert from 'assert'
 import MetricsFactory, { MetricsInterface } from '../src'
-import LoggerFactory from 'metallic-logger'
 
 describe('metrics-factory', function () {
   it('.create() should return a Metrics instance', function () {
-    const logger = undefined
-    const options = { prefix: 'wadus', interval: 0 }
-    const metrics = MetricsFactory.create({ logger, options })
-
-    assert.ok(metrics instanceof MetricsInterface)
-  })
-
-  it('.create() should return a Metrics instance with logger', function () {
-    const logger = LoggerFactory.create({ options: { name: 'wadus' } })
-    const metrics = MetricsFactory.create({ logger, options: { prefix: 'wadus', interval: 0 } })
+    const metrics = MetricsFactory.create({})
 
     assert.ok(metrics instanceof MetricsInterface)
   })
 
   it('.create() should return undefined instance when disabled', function () {
-    const logger = undefined
-    const options = { enabled: false, prefix: 'wadus', interval: 0 }
-    const metrics = MetricsFactory.create({ logger, options })
+    const options = { enabled: false }
+    const metrics = MetricsFactory.create({ options })
 
     assert.ok(metrics === undefined)
   })
